7f81ed8b962f0b28c7fb8ed839e4cfa30a5a9073
Amit <a007@gmail.com>
2020-09-28 20:26:21 +0530

    first commit

---
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.demo.security;
+
+
+public class SecurityConfiguration{
+ /*   public void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/encrypt/**").ignoringAntMatchers("/decrypt/**");
+        super.configure(http);
+    }*/
+}
